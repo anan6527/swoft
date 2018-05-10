271af2b09808ea908f962492f05255e93b0212dc
安智超 <394378627@qq.com>
2018-05-10 10:12:18 +0800

    test

--- swoft/app/Controllers/HttpClientController.php
+++ swoft/app/Controllers/HttpClientController.php
@@ -27,6 +27,7 @@ class HttpClientController
     public function request(): array
     {
         $client = new Client();
+        
         $result = $client->get('http://www.swoft.org')->getResult();
         $result2 = $client->get('http://www.swoft.org')->getResponse()->getBody()->getContents();
         return compact('result', 'result2');
